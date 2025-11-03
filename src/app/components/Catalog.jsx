07737a11690b70c53406a1b3527e63adf03676a0
Sofia Benavides <Sbenavidesarias@usf.edu>
2025-11-02 20:46:07 -0500

    Starting the catalog and linking all folders to the catalog file

---
+++ src/app/components/Catalog.jsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { useEffect, useMemo, useState } from 'react';
+import StatusMessage from './StatusMessage';
+import CategoryFilter from './CategoryFilter';
+import PriceFilter from './PriceFilter';
+import ProductList from './ProductList';
+import CartSummary from './CartSummary';
